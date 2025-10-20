fff7ea370987b46cb8848a675a2a8e220a938387
ErenYea9er69 <rayenfootball.24@gmail.com>
2025-10-20 18:41:08 +0100

    creating recherch file

---
+++ src/app/recherche-par-categorie/recherche-par-categorie.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recherche-par-categorie',
+  imports: [],
+  templateUrl: './recherche-par-categorie.html',
+  styleUrl: './recherche-par-categorie.css'
+})
+export class RechercheParCategorie {
+
+}
