2de8010e0ee8992b8bd1cbe2e21d2de165ff2891
ErenYea9er69 <rayenfootball.24@gmail.com>
2025-10-30 15:06:12 +0100

    tp6 done

--- src/app/add-ai-model/add-ai-model.ts
+++ src/app/add-ai-model/add-ai-model.ts
@@ -1,42 +1,54 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AIModel } from '../model/ai.model';
 import { AIModelService } from '../services/ai';
 import { OpenState } from '../model/OpenState.model';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-ai-model',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './add-ai-model.html',
 })
-export class AddAIModelComponent {
+export class AddAIModelComponent implements OnInit {
 
   newAIModel: AIModel = new AIModel();
   categories! : OpenState[];
   newIdCat! : number;
   newCategorie! : OpenState;
   message: string = '';
+  myAI!: FormGroup;
 
-  constructor(private aiModelService: AIModelService, 
-    private router: Router) {}
+  constructor(
+    private aiModelService: AIModelService, 
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {}
 
-    ngOnInit(): void {
+  ngOnInit(): void {
     this.categories = this.aiModelService.listestate();
+    
+    this.myAI = this.formBuilder.group({
+      idModel: ['', [Validators.required]],
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      version: ['', [Validators.required]],
+      accuracy: ['', [Validators.required]],
+      trainingDate: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      idCat: ['', [Validators.required]]
+    });
   }
 
-
- addAIModel() {
-
+  addAIModel() {
     this.newCategorie = this.aiModelService.consulterCategorie(this.newIdCat);
     this.newAIModel.OpenState = this.newCategorie;
-    if (this.newAIModel.idModel && this.newAIModel.name && this.newAIModel.version && this.newAIModel.accuracy) {
+    
+    if (this.myAI.valid) {
       this.aiModelService.ajouterAIModel(this.newAIModel);
       this.message = "Modèle " + this.newAIModel.name + " ajouté avec succès !";
-
       this.router.navigate(['/aiModels']);
     }
-
   }
 }
--- src/app/update-ai-model/update-ai-model.ts
+++ src/app/update-ai-model/update-ai-model.ts
@@ -2,42 +2,56 @@ import { Component, OnInit } from '@angular/core';
 import { AIModel } from '../model/ai.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AIModelService } from '../services/ai';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-  import { OpenState } from '../model/OpenState.model';
+import { OpenState } from '../model/OpenState.model';
 
 @Component({
   selector: 'app-update-ai-model',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './update-ai-model.html',
 })
 export class UpdateAIModel implements OnInit {
 
-
-categories! : OpenState[];
-updatedCatId! : number;
-
+  categories! : OpenState[];
+  updatedCatId! : number;
   currentAIModel = new AIModel();
-  constructor(private activatedRoute: ActivatedRoute,
+  myAI!: FormGroup;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
     private router :Router,
-     private aiModelService: AIModelService) {}
+    private aiModelService: AIModelService,
+    private formBuilder: FormBuilder
+  ) {}
+
   ngOnInit() {
-    // console.log(this.route.snapshot.params.id);
     this.categories = this.aiModelService.listestate();
 
     this.currentAIModel = this.aiModelService.consulterAIModel(
       this.activatedRoute.snapshot.params['id']);
-      this.updatedCatId=this.currentAIModel.OpenState.idstate;
+    this.updatedCatId=this.currentAIModel.OpenState.idstate;
+
+    this.myAI = this.formBuilder.group({
+      idModel: [{value: '', disabled: true}, [Validators.required]],
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      version: ['', [Validators.required]],
+      accuracy: ['', [Validators.required]],
+      trainingDate: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      idCat: ['', [Validators.required]]
+    });
 
     console.log(this.currentAIModel);
   }
-updateAIModel()
-{ //console.log(this.currentAIModel);
-  this.currentAIModel.OpenState=this.aiModelService.consulterCategorie(this.updatedCatId);
 
-this.aiModelService.updateAIModel(this.currentAIModel);
-this.router.navigate(['aiModels']);
+  updateAIModel() {
+    this.currentAIModel.OpenState=this.aiModelService.consulterCategorie(this.updatedCatId);
 
-}
+    if (this.myAI.valid) {
+      this.aiModelService.updateAIModel(this.currentAIModel);
+      this.router.navigate(['aiModels']);
+    }
+  }
 }
