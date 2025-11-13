fee9889ae5bbad41a974d0b4555ee240e3f198b8
ErenYea9er69 <rayenfootball.24@gmail.com>
2025-11-13 18:27:29 +0100

    97 auth server page all

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { User } from '../model/user.model';
+import { Auth } from '../services/auth';
+import { Router } from '@angular/router';
+
 
 
 @Component({
@@ -12,14 +15,16 @@ import { User } from '../model/user.model';
 })
 export class Login implements OnInit {
 
-user = new User();
+  user = new User();
+  erreur: boolean = false;
 
-ngOnInit() {}
+  ngOnInit() {}
 
-
-  constructor() { }
-onLoggedin()
-{
-console.log(this.user);}
-
+  constructor(private authService: Auth, private router: Router) {}
+  onLoggedin() {
+    let isValidUser: Boolean = this.authService.SignIn(this.user);
+    if (isValidUser) this.router.navigate(['/']);
+    else //alert('Login ou mot de passe incorrecte!');
+      this.erreur = true;
+  }
 }
