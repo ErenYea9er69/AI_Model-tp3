a583224d0c09e3e1df4050a2b60ed3c13f0e5cae
ErenYea9er69 <rayenfootball.24@gmail.com>
2025-11-23 17:42:21 +0100

    before mini projet

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
 export class Auth {
   users: User[] = [
     { username: 'admin', password: '123', roles: ['ADMIN'] },
-    { username: 'nadhem', password: '123', roles: ['USER'] },
+    { username: 'rayen', password: '123', roles: ['USER'] },
   ];
 
   public loggedUser: string | undefined;
